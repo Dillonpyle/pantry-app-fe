53e252ac2ac77854776a0cad8a2eace78741e54e
Computer <computer@Computers-MacBook-Pro.local>
2019-03-01 13:22:20 -0700

    css

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 import { withRouter, Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import img from './pantry1.jpeg'
+import './home.css'
+
 
 
 const index = ({ username }) => {
     return (
-        <div>
+        <div id='homeBody'>
             <h1 id='homeHeader'>{username}'s Pantry</h1>
-            <div>
-                <Link to={'/ingredients'}><button>Ingredients</button> </Link>
-                <Link to={'/recipes'}> <button>Recipe</button> </Link>
-                <button>Add Recipe</button>
+            <div id='homePageButtons'>
+                <Link to={'/ingredients'}><Button outline color="primary">Ingredients</Button> </Link>
+                <Button outline color="primary"><Link to={'/recipes'}> Recipe </Link></Button>
+                <Button outline color="primary">Add Recipe</Button>
             </div>
+            <img id='homeImg' src={img} />
         </div>
     )
 }
