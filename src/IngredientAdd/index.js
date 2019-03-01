2dcff847e1a026076c3e262a8a0e1055fa6dff37
Sam Brown <stbrown463@gmail.com>
2019-02-28 19:58:55 -0600

    can add new ingredients to db on front end

---
+++ src/IngredientAdd/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+class IngredientAdd extends Component {
+	constructor () {
+		super ()
+
+		this.state = {
+			name: '',
+			typeof: ''
+		}
+	}
+
+	addIngredient = async (e) => {
+		e.preventDefault()
+		console.log('hitting addIngredinet')
+		try {
+			const response = await fetch (`${process.env.REACT_APP_API_URL}/ingredients`, {
+				method: 'POST',
+				headers: {
+					"Content-Type": "application/json",
+				},
+				credentials: 'include',
+				body: JSON.stringify(this.state)
+			})
+
+			if (!response.ok) {
+				throw Error (response.statusText)
+			}
+
+			const parsedResponse = await response.json()
+			console.log(parsedResponse)
+
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]:e.target.value
+		})
+	}
+
+	render () {
+		return (
+			<div>
+				<h2>Add a new ingredient</h2>
+				<form onSubmit={this.addIngredient}>
+					<input name="name" placeholder="Ingredient name..." value={this.state.name} onChange={this.handleChange}/>
+					<input name="typeof" placeholder="Ingredient type..." value={this.state.type} onChange={this.handleChange}/>
+					<button>Add new Ingredient</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default IngredientAdd
