94b215a28fad11d3e20bcd771a2641f665a3db78
Computer <computer@Computers-MacBook-Pro.local>
2019-03-04 08:56:12 -0700

    css on ingredients page

--- src/IngredientsResults/index.js
+++ src/IngredientsResults/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './IngredientsResults.css'
 
 const IngredientsResults = (props) => {
 	console.log(props);
 	return (
-		<div>
+		<div id="ingredientsResults">
 			<div className="left">
 				{props.results.ing_name}: {props.results.ing_type}
 			</div>
 			<button className="right" onClick={props.addIngredient.bind(null, props.results.ing_id)}>
-				Add
+				Add To Your Pantry
 			</button>
 
 
