c949146d7b3b74326b8486834dcf8cbbb974aebd
Sam Brown <stbrown463@gmail.com>
2019-03-04 09:41:01 -0600

    css changes

--- src/Recipes/Form.js
+++ src/Recipes/Form.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import RecipeAdd from '../RecipeAdd'
+import './recipe.css'
 
 
 const Form = props => (
     <React.Fragment>
-        <RecipeAdd user={props}/>
-        <h3>Search for a recipe online!</h3>
-        <form onSubmit={props.getRecipe} >
-            <input type="text" name="recipeName" placeholder="Search..." />
-            <button>search</button>
-        </form>
-        <div>
-            {props.recipes.map((recipe, i) => {
-                return (<div key={i} >
-                    <div >
-                        <img src={recipe.image_url} alt={recipe.title} />
-                        <p>{recipe.title}</p>
-                        <a href={recipe.source_url} target="_blank">source link</a>
-                    </div >
-                </div>)
-            })}
+        <div className="main-container">
+            <RecipeAdd user={props}/>
+            <h3>Search for a recipe online!</h3>
+            <form onSubmit={props.getRecipe} >
+                <input type="text" name="recipeName" placeholder="Search..." />
+                <button>search</button>
+            </form>
+            <div>
+                {props.recipes.map((recipe, i) => {
+                    return (<div key={i} >
+                        <div >
+                            <img src={recipe.image_url} alt={recipe.title} />
+                            <p>{recipe.title}</p>
+                            <a href={recipe.source_url} target="_blank">source link</a>
+                        </div >
+                    </div>)
+                })}
+            </div>
         </div>
         
     </React.Fragment>
